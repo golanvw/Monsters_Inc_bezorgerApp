57d8c1fea78a1ded65200c25d42e29111b7e18eb
TyrannousGraph9 <2405594cremers@zuyd.nl>
2026-06-15 11:43:33 +0200

    Login page created, adde red color in the styling sheet.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,17 +7,28 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+		ErrorLabel.IsVisible = false;
+    }
 
-	private void OnCounterClicked(object? sender, EventArgs e)
+	private void OnLoginClicked(object? sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+		if(UsernameInput.Text == "admin" && PasswordInput.Text == "admin")
+		{
+			ErrorLabel.IsVisible = false;
+            DisplayAlert("Login Gelukt", "Je bent ingelogd!", "OK");
+        }
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+		{
+			ErrorLabel.IsVisible = true;
+			UsernameInput.BackgroundColor = Color.FromArgb("#FF0000");
+			UsernameInput.TextColor = Color.FromArgb("#FFFFFF");
+			PasswordInput.BackgroundColor = Color.FromArgb("#FF0000");
+			PasswordInput.TextColor = Color.FromArgb("#FFFFFF");
+        }
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+	private void OnForgotPasswordClicked(object? sender, EventArgs e)
+	{
+		DisplayAlert("Wachtwoord vergeten", "Neem contact op met de beheerder om je wachtwoord te resetten.", "OK");
+    }
 }
